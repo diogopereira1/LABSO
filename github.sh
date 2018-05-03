ae81760db36bdcfcc596ed5684123795f55d8de9
diogopereira1 <diogo.pereira@soulasalle.com.br>
2018-05-02 21:44:41 -0300

    via script

--- github.sh
+++ github.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-echo "Informe usuario: "
-read usuario
-
-echo "Informe email: "
-read email
-
-git config --global user.name $usuario
-git config --global user.email $email
+git config --global user.name diogopereira1
+git config --global user.email diogo.pereira@soulasalle.com.br
 
 git add *
-
 git commit -m "via script"
-
 git push
