a770858f25a2418f36f959be6e761b1f021e0b6a
diogopereira1 <diogo.pereira@soulasalle.com.br>
2018-05-02 21:40:03 -0300

    via script

--- github.sh
+++ github.sh
@@ -6,8 +6,8 @@ read usuario
 echo "Informe email: "
 read email
 
-$ git config --global user.name $usuario
-$ git config --global user.email $email
+git config --global user.name $usuario
+git config --global user.email $email
 
 git add *
 
